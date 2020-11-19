2678ea5558a958faeca18989f5b29ed78f44f0b8
jeffer <chaustrejefferson@gmail.com>
2020-11-19 11:04:06 -0500

    proyecto final - beta

---
+++ src/public/js/app.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.griv-etapas',);
+ScrollReveal().reveal('.img-bioelementos');
+ScrollReveal().reveal('.tabla');
+ScrollReveal().reveal('.saludable');
+ScrollReveal().reveal('.oligoelementos');
